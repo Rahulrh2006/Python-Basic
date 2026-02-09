7ed422dfcdb67b41bcd1df8845ef75f12e5597bc
Rahulrh2006 <rahulharagapure2006@gmail.com>
2026-02-10 00:06:36 +0530

    Add files via upload

---
+++ problem-3.py
@@ -0,0 +1,9 @@
+#program to check weather the number is greater then 100
+
+
+a=int (input ("enter anumber:"))
+if a>100:
+    print("the number is greater than 100")
+
+else:
+    print("the number is not greater than 100")
