8715596e0ea953a8a16ef69e86ce88f6983e1988
Rahulrh2006 <rahulharagapure2006@gmail.com>
2026-02-16 23:13:13 +0530

    Add files via upload

---
+++ problem-6.py
@@ -0,0 +1,19 @@
+num = 2
+while (num <= 20) :
+  print(num) 
+num = num + 2
+
+num = 1
+while(num<=20):
+  print(num) 
+num = num + 2
+
+num = 1
+while (num <= 10) :
+ print(num) 
+num = num + 1
+
+num = 0
+while(num<10):
+ print(num) 
+num = num + 1
