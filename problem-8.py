9e5f869f8baa66b638426fe4e7b029ed79943190
Rahulrh2006 <rahulharagapure2006@gmail.com>
2026-02-19 22:41:16 +0530

    Add files via upload

---
+++ problem-8.py
@@ -0,0 +1,16 @@
+#number triangle using python
+
+rows = 4
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+#right angle traingle
+
+rows = 4
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
