a8692ed91fc8f07c8a6aa56738a01fe4a5a32aa2
Rahulrh2006 <rahulharagapure2006@gmail.com>
2026-03-11 22:13:48 +0530

    Add files via upload

---
+++ problem-15.py
@@ -0,0 +1,13 @@
+#demonstrate break and continue statments 
+#break
+for i in range(1,11):
+    if i== 8 :
+        break 
+    print(i)
+
+
+#continue
+for i in range (1 ,11):
+  if i==8 :
+    continue 
+print(i)
